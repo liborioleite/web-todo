a70ca8341666c6d38decdaf89965562bf948019f
Liborio <liborioleite@gmail.com>
2024-01-23 22:01:50 -0300

    initiate project in nuxt and implemented tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="d-flex flex-row text-center hover:bg-blue-500">
+    <!-- <button class="border border-blue-500 rounded-md text-blue-500 bg-white px-2 py-1 mx-2 my-1">Button</button> -->
+    <p class="bg-red-500 font-extrabold hover:bg-blue-500">Eu amo a Ivana</p>
   </div>
 </template>
